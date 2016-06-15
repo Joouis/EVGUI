7e588c32d9ea7cf92a8d27becb0bab090ba49259
Joou <luckyjoou@gmail.com>
2016-06-15 20:18:19 +0800

    Remove the commented code about cursor

--- apps/main.c
+++ apps/main.c
@@ -5,53 +5,13 @@
 
 twin_fbdev_t *tf;
 
-static unsigned int twin_def_cursor_image[] = {
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x88ffffff, 0xff000000, 0xff000000, 0x88ffffff,
-        0x00000000, 0x88ffffff, 0x88ffffff, 0x00000000,
-};
-
-static twin_pixmap_t *twin_get_default_cursor(int *hx, int *hy)
-{
-    twin_pixmap_t *cur;
-    twin_pointer_t data;
-
-    data.argb32 = twin_def_cursor_image;
-    cur = twin_pixmap_create_const(TWIN_ARGB32, 4+4, 4+4, 16+16, data);
-    if (cur == NULL)
-        return NULL;
-    *hx = *hy = 4;
-    return cur;
-}
-
 int main(void)
 {
-	/* int hx, hy; */
-	/* twin_pixmap_t *cur; */
 
 	tf = twin_fbdev_create();
 	if ( tf == NULL ) {
 		return 1;
 	}
-	/* cur = twin_get_default_cursor(&hx, &hy); */
-	/* if (cur != NULL) */
-	/* 	twin_screen_set_cursor(tf->screen, cur, hx, hy); */
 	/* twin_screen_set_background (tf->screen, twin_make_pattern ()); */
 
 #if 0
